09a8ae9fbdbb8721998f75303b5ae47c047714c7
Doan Hieu <hieudx72@gmail.com>
2020-06-23 18:59:32 +0700

    ...

--- QLKhachSan/Program.cs
+++ QLKhachSan/Program.cs
@@ -16,8 +16,8 @@ namespace QLKhachSan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
-           // Application.Run(new FormThuePhong());
+            //Application.Run(new FormLogin());
+            Application.Run(new FormThuePhong());
         }
     }
 }
